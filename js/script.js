3ec1e2d2231e4e9f092c78010b1a09049566ef7c
BRIAN HICKS <bhicks@risd.edu>
2015-06-21 22:21:33 -0700

    Upload

--- js/script.js
+++ js/script.js
@@ -6,131 +6,94 @@ $(document).ready(function(){
 		//jQuary + css selector + action
 	$('.rio').mouseover(function(){
 		$('#rioImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.rio').mouseout(function(){
 		$("#rioImg").hide( 10 );
-		console.log("class hidden");
 	});
 	$('.blend').mouseover(function(){
 		$('#blendImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.blend').mouseout(function(){
 		$("#blendImg").hide( 10 );
-		console.log("class hidden");
 	});
 	$('.nike').mouseover(function(){
 		$('#nikeImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.nike').mouseout(function(){
 		$("#nikeImg").hide( 10 );
-		console.log("class hidden");
 	});
 	$('.poster').mouseover(function(){
 		$('#posterImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.poster').mouseout(function(){
 		$("#posterImg").hide( 10 );
-		console.log("class hidden");
 	});
 	$('.calvino').mouseover(function(){
 		$('#calvinoImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.calvino').mouseout(function(){
 		$("#calvinoImg").hide( 10 );
-		console.log("class hidden");
 	});
 	$('.risdgd').mouseover(function(){
 		$('#risdgdImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.risdgd').mouseout(function(){
 		$("#risdgdImg").hide( 10 );
-		console.log("class hidden");
 	});
 	$('.synergy').mouseover(function(){
 		$('#synergyImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.synergy').mouseout(function(){
 		$("#synergyImg").hide( 10 );
-		console.log("class hidden");
 	});
-
 	$('.bunnies').mouseover(function(){
 		$('#bunniesImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.bunnies').mouseout(function(){
 		$("#bunniesImg").hide( 10 );
-		console.log("class hidden");
 	});
-
 	$('.clinton').mouseover(function(){
 		$('#clintonImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.clinton').mouseout(function(){
 		$("#clintonImg").hide( 10 );
-		console.log("class hidden");
 	});
-
 	$('.transit').mouseover(function(){
 		$('#transitImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.transit').mouseout(function(){
 		$("#transitImg").hide( 10 );
-		console.log("class hidden");
 	});
-
 	$('.buy').mouseover(function(){
 		$('#buyImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.buy').mouseout(function(){
 		$("#buyImg").hide( 10 );
-		console.log("class hidden");
 	});
-
 	$('.dump').mouseover(function(){
 		$('#dumpImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.dump').mouseout(function(){
 		$("#dumpImg").hide( 10 );
-		console.log("class hidden");
 	});
 
 	$('.filthy').mouseover(function(){
 		$('#filthyImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.filthy').mouseout(function(){
 		$("#filthyImg").hide( 10 );
-		console.log("class hidden");
 	});
-
 	$('.openstudio').mouseover(function(){
 		$('#openstudioImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.openstudio').mouseout(function(){
 		$("#openstudioImg").hide( 10 );
-		console.log("class hidden");
 	});
-
 	$('.holiday').mouseover(function(){
 		$('#holidayImg').show( 10 );
-		console.log("class shown");
 	});
 	$('.holiday').mouseout(function(){
 		$("#holidayImg").hide( 10 );
-		console.log("class hidden");
 	});
 	
 });

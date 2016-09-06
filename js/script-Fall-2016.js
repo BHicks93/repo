d5ec7b61ffa5824386f0e691b1bd98cdd16f3079
BRIAN HICKS <bhicks@risd.edu>
2016-09-06 11:40:24 -0400

    bye box

--- js/script-Fall-2016.js
+++ js/script-Fall-2016.js
@@ -3,14 +3,6 @@ var boxCenter=[box.offset().left+box.width()/2, box.offset().top+box.height()/2]
 
 $(document).ready(function() {
 
-	$(document).mousemove(function(e){    
-		var angle = Math.atan2(e.pageX- boxCenter[0],- (e.pageY- boxCenter[1]) )*(180/Math.PI);	    
-	    
-	    box.css({ "-webkit-transform": 'rotate(' + angle + 'deg)'});    
-	    box.css({ '-moz-transform': 'rotate(' + angle + 'deg)'});
-	    box.css({ 'transform': 'rotate(' + angle + 'deg)'});
-	    });
-
 	$(window).resize(function() {
     	$('#top').width($(this).width());
 	}).resize();

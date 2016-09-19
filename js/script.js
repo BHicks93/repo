dab0bd1e5ca3d32db97753d7d58afdd4e4e47edf
BRIAN HICKS <bhicks@risd.edu>
2016-09-19 13:17:40 -0400

    updated

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
-var box=$(".box");
-var boxCenter=[box.offset().left+box.width()/2, box.offset().top+box.height()/2];
+$(document).ready(function() {
+	$( "#project1, #project2, #project3, #project4, #project5, #project6, #project7, #project8" ).hover(
+		function() {
+  			$(".text", this).css( "color", "white" );
+  			$(".overlap-box", this).css( "background", "white" );
+  		}, function() { 
+  			$(".text", this).css( "color", "transparent" );
+  			$(".overlap-box", this).css( "background", "transparent" );
+  		}
+	);
 
-$(document).mousemove(function(e){    
-    
-        
-	var angle = Math.atan2(e.pageX- boxCenter[0],- (e.pageY- boxCenter[1]) )*(180/Math.PI);	    
-    
-    box.css({ "-webkit-transform": 'rotate(' + angle + 'deg)'});    
-    box.css({ '-moz-transform': 'rotate(' + angle + 'deg)'});
-    box.css({ 'transform': 'rotate(' + angle + 'deg)'});
-    });
+	$( "#side-bar, .side-name" )
+		.click(function() {
+			$(".projects").addClass('projects-left');
+			$("#side-bar").css( "width", "400px" );
+			$("nav").addClass('show');
+			$(".project-list").addClass('show');
+			$(".close").addClass('show');
+			$(".side-name").removeClass('show');
+	});
+	$( ".close, .projects" )
+		.click(function() {
+			$(".projects").removeClass('projects-left');
+			$("#side-bar").css( "width", "40px" );
+			$("nav").removeClass('show');
+			$(".project-list").removeClass('show');
+			$(".side-name").addClass('show');
+			$(".close").addClass('hide');
+	});
+	$( "#info-trigger" ).on( "click", function() {
+		$(".info-page").toggleClass('show'); 
+	});
+	$( "#index-wrapper" ).css( "width", $( window ).width() );
+	$( window ).resize(function() {
+	  $( "#index-wrapper" ).css( "width", $( window ).width() );
+	});
+});

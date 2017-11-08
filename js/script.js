a9ad4f1051c33a6b8340cca7b7976a5df4422aad
Brian Hicks <bhicks@risd.edu>
2017-11-08 18:37:44 -0500

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$( "#trigger" ).on( "click", function() {
-		$(".info-page").toggleClass("show-info-page", 1000, "easeOutSine");
-	});
-	$( "#trigger" ).on( "click", function() {
-		$("menu-icon").toggleClass("rotateBack", 1000, "easeInOutQuint");
-	});
-});
